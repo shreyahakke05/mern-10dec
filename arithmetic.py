fd813d8967b26548e2f8a072f4c330a6b1cfa837
Shreya Hakke <shreyahakke@gmail.com>
2025-12-11 11:22:42 +0530

    add subtract multiply division

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+
+def add(a,b):
+    print("Add:",a+b)
+
+def sub(a,b):
+    print("Sub:",a-b)
+
+def mul(a,b):
+    print("Multiply:",a*b)
+
+def div(a,b):
+    print("Division:",a/b)
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import arithmetic as ar
+import geometry as geo
+from geometry import calc_rec_peri as peri
+from geometry import calc_rec_area as area
+
 print("Hello,World!!")
+
+num1=int(input("Enter num1:"))
+num2=int(input("Enter num2:"))
+
+ar.add(num1,num2)
+ar.sub(num1,num2)
+ar.mul(num1,num2)
+ar.div(num1,num2)
+
+len=int(input("Enter length:"))
+br=int(input("Enter breadth:"))
+peri(num1,num2)
+area(num1,num2)
